a05e6d66386c52e32e32847d8e6a78d3493fb920
CoralLeiCN <19520698+CoralLeiCN@users.noreply.github.com>
2025-08-04 21:04:40 +0100

    Refactor conversation handling to use chat_with_mistral and update Gradio interface

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 import gradio as gr
-from nomollm_agent.client import get_completion
+
+from nomollm_agent.client import chat_with_mistral
+from nomollm_agent.tools import add, get_weather
+
+tools = [add.info, get_weather.info]
 
 
-def llm_response(message, history):
-    return get_completion(message)
+def multi_turns_conversation(message, history):
+    keys_to_keep = ["role", "content"]
+    history = [{k: d[k] for k in keys_to_keep if k in d} for d in history]
+    history.append({"role": "user", "content": message})
+
+    # Get LLM response
+    llm_response = chat_with_mistral(history, tools)
+    return llm_response[-1].content
 
 
 if __name__ == "__main__":
-    gr.ChatInterface(llm_response).launch()
+    # Remove type="messages" as it's not needed for the corrected implementation
+    gr.ChatInterface(multi_turns_conversation, type="messages").launch()
